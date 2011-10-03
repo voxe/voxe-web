73955a44f1cc994de892a532d117642731c18ab7
Benjamin LAN SUN LUK <lsl.benjamin@gmail.com>
2011-10-03 11:58:53 +0200

    Initial commit with a Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joinplato::Application.config.secret_token = '0117d56144e16958533552f42e577835337a7a2fd70c43df94dc9d47b76ed85502a1ffcfff7d39bab7b2426ec95d7edfca7e1800a43a984b52de2f560e001cea'
