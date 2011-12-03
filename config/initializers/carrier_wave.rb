3ead665456a574c08ddcd5c910bee19ac168ecbc
Benjamin LAN SUN LUK <lsl.benjamin@gmail.com>
2011-12-03 14:20:49 +0100

    Use heroku config vars to store S3 creds

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIJ4P4FKYJGEO2D6Q',
-    :aws_secret_access_key  => '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',
-    :region                 => 'eu-west-1'
+    :aws_access_key_id      => ENV['S3_KEY'],
+    :aws_secret_access_key  => ENV['S3_SECRET'],
   }
   config.fog_directory  = 'joinplato'
 end
