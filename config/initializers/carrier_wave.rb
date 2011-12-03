19f4841b4ca482b820d67bc1c7ffe910a43fff24
Benjamin LAN SUN LUK <lsl.benjamin@gmail.com>
2011-12-03 14:35:40 +0100

    Update the bucket name

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET'],
   }
-  config.fog_directory  = 'joinplato'
+  config.fog_directory  = 'thevoxe'
 end
