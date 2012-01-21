8f2fd162bfc174c2a9fd96af75f4387c928534eb
Paul Chobert <paul@chobert.fr>
2012-01-21 19:05:04 +0100

    API disable election caching

--- app/controllers/api/v1/elections_controller.rb
+++ app/controllers/api/v1/elections_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ElectionsController < Api::V1::ApplicationController
 
-  caches_action :show, :expires_in => 1.hour
+  #caches_action :show, :expires_in => 1.hour
 
   # GET /api/v1/elections/1
   def show
@@ -28,7 +28,7 @@ class Api::V1::ElectionsController < Api::V1::ApplicationController
   # PUT /api/v1/elections/1
   def update
     if @election.update_attributes params[:election]
-      expire_action action: "show", id: @election.id
+      #expire_action action: "show", id: @election.id
       
       render 'api/v1/elections/show.rabl', status: :ok
     else
@@ -46,7 +46,7 @@ class Api::V1::ElectionsController < Api::V1::ApplicationController
     @election_tag = ElectionTag.new election: @election, tag: tag, parent_tag: parent_tag
 
     if @election_tag.save
-      expire_action action: "show", id: @election.id
+      #expire_action action: "show", id: @election.id
       render 'api/v1/elections/show.rabl', status: :created
     else
       render text: {errors: @election_tag.errors}.to_json, status: :unprocessable_entity, layout: 'api_v1'
@@ -59,7 +59,7 @@ class Api::V1::ElectionsController < Api::V1::ApplicationController
     @election.candidacies.build candidates: candidates
 
     if @election.save
-      expire_action action: "show", id: @election.id
+      #expire_action action: "show", id: @election.id
       render 'api/v1/elections/show.rabl', status: :created
     else
       render text: {errors: @election.errors}.to_json, status: :unprocessable_entity, layout: 'api_v1'
@@ -69,7 +69,7 @@ class Api::V1::ElectionsController < Api::V1::ApplicationController
   # DELETE /api/v1/elections/1/removetag
   def removetag
     @election.election_tags.where(tag_id: params[:tagId]).destroy_all
-    expire_action action: "show", id: @election.id
+    #expire_action action: "show", id: @election.id
     head :ok
   end
   
