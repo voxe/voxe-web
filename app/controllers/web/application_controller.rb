9ab62ba6161f7677f82b0483ced67e02da382470
Paul Chobert <paul@chobert.fr>
2012-02-06 04:59:49 +0100

    show only published cancidacies on all web controllers

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -6,6 +6,7 @@ class Web::ApplicationController < ApplicationController
   
   private
     def set_election
+      @only_published_candidacies = true
       @election = Election.first conditions: {namespace: params[:namespace]}
       # returns 404 if election does not exist
       return not_found unless @election
--- app/controllers/web/elections_controller.rb
+++ app/controllers/web/elections_controller.rb
@@ -5,7 +5,6 @@ class Web::ElectionsController < Web::ApplicationController
   def show
     respond_to do |format|
       format.html do
-        @only_published_candidacies = true
         @options = render_to_string('/api/v1/elections/show.json', layout: false)
         render "web/application/index"
       end
