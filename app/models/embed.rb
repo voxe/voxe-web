064330329613207ed85817fe4b9c8d40416d9fdb
Pierre Valade <pierre@valade.info>
2012-02-05 23:17:34 -0500

    webviews: added videos (tested on iPhone for now)

--- app/models/embed.rb
+++ app/models/embed.rb
@@ -20,6 +20,22 @@ class Embed
   #
   validates_presence_of :url
   validate :checkout_embed, if: :url_changed?
+  
+  # SUNDAY HACK !! WILL BE CHANGED IN THE FUTURE
+  def youtube_id
+    rexp = /v=(.*)/
+    rexp.match(url).captures.first
+  end
+  
+  def video_player
+    # http://apiblog.youtube.com/2009/02/youtube-apis-iphone-cool-mobile-apps.html
+    "<object width='270' height='200'>
+    <param name='movie' value='http://www.youtube.com/v/#{youtube_id}'></param>
+    <param name='wmode' value='transparent'></param>
+    <embed src='http://www.youtube.com/v/#{youtube_id}'
+    type='application/x-shockwave-flash' wmode='transparent' width='270' height='200'></embed>
+    </object>"
+  end
 
 
   private
