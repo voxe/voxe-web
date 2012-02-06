ab87b42d92d8280bcbb23762681ddaa2c41f008f
Pierre Valade <pierre@valade.info>
2012-02-06 00:38:47 -0500

    web: fixing bug not fetching all tags

--- app/controllers/web/elections_controller.rb
+++ app/controllers/web/elections_controller.rb
@@ -5,6 +5,8 @@ class Web::ElectionsController < Web::ApplicationController
   def show
     respond_to do |format|
       format.html do
+        @only_published_candidacies = true
+        @all_tags = true
         @options = render_to_string('/api/v1/elections/show.json', layout: false)
         render "web/application/index"
       end
