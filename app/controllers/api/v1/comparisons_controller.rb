7bd18898c63868e7373c360bd31ee9961b99fa19
Pierre Valade <pierre@valade.info>
2012-02-15 20:57:36 -0500

    HP: fixing bugs, adding cache and new image

--- app/controllers/api/v1/comparisons_controller.rb
+++ app/controllers/api/v1/comparisons_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::ComparisonsController < Api::V1::ApplicationController
   
   skip_load_and_authorize_resource
   
+  caches_action :search, expires_in: 300.seconds, if: Proc.new { params[:afterTimestamp].blank? }
+  
   def search
     # returns the last 20 comparisons
     @events = Event.where(name: "comparison").desc(:created_at).limit(20)
