ddc60ae7afb322b7e8be98cfc2ef154fbae75922
Pierre Valade <pierre@valade.info>
2012-02-24 08:11:03 -0500

    cache election embed

--- app/controllers/embed/elections_controller.rb
+++ app/controllers/embed/elections_controller.rb
@@ -2,6 +2,9 @@ class Embed::ElectionsController < ApplicationController
   
   layout false
   
+  # will be reset every deploy
+  caches_action :show
+  
   def show
     begin
       @election = Election.find params[:id]
