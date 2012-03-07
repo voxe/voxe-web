c1094e34e176b463403654cfecb97ab02abf67de
Pierre Valade <pierre@foursquare.com>
2012-03-07 14:30:53 -0500

    disabled comparisons/recent

--- app/controllers/api/v1/comparisons_controller.rb
+++ app/controllers/api/v1/comparisons_controller.rb
@@ -6,10 +6,11 @@ class Api::V1::ComparisonsController < Api::V1::ApplicationController
   
   def search
     # returns the last 20 comparisons
-    @events = Event.where(name: "comparison").desc(:created_at).limit(20)
-    unless params[:afterTimestamp].blank?
-      @events = @events.where :created_at.gt => Time.at(params[:afterTimestamp].to_i)
-    end
+    @events = []
+    # @events = Event.where(name: "comparison").desc(:created_at).limit(20)
+    # unless params[:afterTimestamp].blank?
+    #   @events = @events.where :created_at.gt => Time.at(params[:afterTimestamp].to_i)
+    # end
   end
   
 end
