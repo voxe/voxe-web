0e4bf2cbcacc4ebffb12e7565472b08a8595c52e
Benjamin LAN SUN LUK <lsl.benjamin@gmail.com>
2012-03-09 20:33:39 +0100

    Disable identity_map for all environments + reenable comparisons#search

--- app/controllers/api/v1/comparisons_controller.rb
+++ app/controllers/api/v1/comparisons_controller.rb
@@ -6,11 +6,10 @@ class Api::V1::ComparisonsController < Api::V1::ApplicationController
   
   def search
     # returns the last 20 comparisons
-    @events = []
-    # @events = Event.where(name: "comparison").desc(:created_at).limit(20)
-    # unless params[:afterTimestamp].blank?
-    #   @events = @events.where :created_at.gt => Time.at(params[:afterTimestamp].to_i)
-    # end
+    @events = Event.where(name: "comparison").desc(:created_at).limit(20)
+    unless params[:afterTimestamp].blank?
+      @events = @events.where :created_at.gt => Time.at(params[:afterTimestamp].to_i)
+    end
   end
   
 end
