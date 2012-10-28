d85d69375ffff1e10d8e0a9d1efc1c9ceca30c76
Benjamin LAN SUN LUK <lsl.benjamin@gmail.com>
2012-10-28 18:36:36 +0100

    Change layout for login + Bug fix on password recovery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     authenticate_user!
   end
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
   
   private
     
