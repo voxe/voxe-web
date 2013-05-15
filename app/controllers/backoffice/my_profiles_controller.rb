fd6eed1d623b38a6c338c9d1dc4a18c4b4e67123
Xavier Cavarelli <xaviercavarelli@gmail.com>
2013-05-15 12:12:35 +0200

    Creation of a candidacy's profile if none existed

--- app/controllers/backoffice/my_profiles_controller.rb
+++ app/controllers/backoffice/my_profiles_controller.rb
@@ -1,9 +1,21 @@
 class Backoffice::MyProfilesController < Backoffice::BackofficeController
   before_filter :load_profile
-  #load_and_authorize_resource :candidacy_candidate_profile
 
   def show
-    redirect_to action: :edit
+    if @profile.nil?
+      redirect_to action: :new
+    else
+      redirect_to action: :edit
+    end
+  end
+
+  def new
+    @profile = CandidacyCandidateProfile.new
+  end
+
+  def create
+    @profile = current_candidacy.create_candidate_profile(params[:candidacy_candidate_profile])
+    respond_with @profile, location: backoffice_my_profile_path
   end
 
   def edit
