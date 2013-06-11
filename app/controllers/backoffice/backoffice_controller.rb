248af70b4df0f17780ff9732e0560c12230296da
Xavier Cavarelli <xaviercavarelli@gmail.com>
2013-06-11 11:13:44 +0200

    User authentification for backoffice

--- app/controllers/backoffice/backoffice_controller.rb
+++ app/controllers/backoffice/backoffice_controller.rb
@@ -1,6 +1,7 @@
 class Backoffice::BackofficeController < ApplicationController
   layout 'backoffice'
   respond_to :html, :json
+  before_filter :authenticate_user!
 
   helper_method :current_candidacy, :current_candidate
 
@@ -11,10 +12,6 @@ class Backoffice::BackofficeController < ApplicationController
 
   def current_ability
     @_current_ability ||= BackofficeAbility.new(current_user)
-  end
-
-  def current_user
-    @_current_user ||= User.where(email: /xavier/).first
   end
 
   def current_candidacy
