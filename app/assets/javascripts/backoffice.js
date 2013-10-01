22c85d6f30900c90427f72b2091fa1d0277ef531
Benjamin Lan Sun Luk <lsl.benjamin@gmail.com>
2013-10-02 00:55:19 +0200

    Front of candidate BO by Edouard

--- app/assets/javascripts/backoffice.js
+++ app/assets/javascripts/backoffice.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require select2
+//= require bootstrap-carousel
+//= require bootstrap-transition
 //= require_tree ./backoffice/propositions
 //= require_tree ./backoffice/lib
 //= require_tree ./backoffice
