3260ca732ab3aca0833397050132b8e6ccee96a9
Benjamin Lan Sun Luk <lsl.benjamin@gmail.com>
2014-07-11 22:37:24 +0200

    Tag uploader

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -16,7 +16,7 @@ class IconUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "icons/#{model.id}"
+    "tags"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -24,44 +24,34 @@ class IconUploader < CarrierWave::Uploader::Base
     "/images/icons/tag_#{version_name}.png"
   end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
-  
-  # Create different versions of your uploaded files:
   version :small do
     process :resize_to_fill => [32, 32]
     process :convert => 'png'
+    def full_filename for_file = model.icon.file
+      if model and model.namespace.present?
+        "v2-#{model.namespace}_32.png"
+      end
+    end
   end
-  
+
   version :medium do
     process :resize_to_fill => [64, 64]
     process :convert => 'png'
-  end
-  
-  version :large do
-    process :resize_to_fill => [256, 256]
-    process :convert => 'png'
+    def full_filename for_file = model.icon.file
+      if model and model.namespace.present?
+        "v2-#{model.namespace}_64.png"
+      end
+    end
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "original.jpg" if original_filename
+    if model and model.namespace.present?
+      "v2-#{model.namespace}_original.png"
+    end
   end
 
 end
